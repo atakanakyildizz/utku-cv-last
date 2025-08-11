fdc050151a2cfb8239efe0499c034508a40f9e29
Atakan Akyıldız <atakanakyildiz0@gmail.com>
2025-08-11 23:39:24 +0300

    changes

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,22 +1,24 @@
-import {FC, memo} from 'react';
+import { FC, memo } from 'react';
 
-export interface IconProps extends React.HTMLAttributes<SVGSVGElement> {
+export interface IconProps extends React.SVGProps<SVGSVGElement> {
   svgRef?: React.Ref<SVGSVGElement>;
-  transform?: string;
 }
 
-const Icon: FC<IconProps> = memo(({children, className, svgRef, transform, ...props}) => (
-  <svg
-    className={className}
-    fill="currentColor"
-    ref={svgRef}
-    transform={transform}
-    viewBox="0 0 128 128"
-    width="128"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}>
-    {children}
-  </svg>
-));
+const Icon: FC<IconProps> = memo(
+  ({ children, className, svgRef, viewBox = '0 0 128 128', width = 128, fill = 'currentColor', ...props }) => (
+    <svg
+      ref={svgRef}
+      className={className}
+      viewBox={viewBox}
+      width={width}
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}   // height, transform, stroke, etc. buradan gelebilir
+    >
+      {children}
+    </svg>
+  )
+);
 
 export default Icon;
+
